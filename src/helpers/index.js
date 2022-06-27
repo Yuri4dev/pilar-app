e1018e266028e6510e757c5499ba0e3b11cbabbd
Yuri Adriel <yuriadriel.43@gmail.com>
2022-06-26 21:27:32 -0300

    fix: add padStart to fix date

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,12 @@
 export const formatDate = (date) => {
   const data = new Date(date);
-  return data.getDate() + "/" + data.getMonth() + "/" + data.getFullYear();
+  return (
+    data.getDate().toString().padStart(2, "0") +
+    "/" +
+    String(data.getMonth() + 1).padStart(2, "0") +
+    "/" +
+    data.getFullYear()
+  );
 };
 export const slugify = (text) => {
   return text.toString().normalize("NFKD").trim();
