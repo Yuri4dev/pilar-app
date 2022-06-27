9a42f6b2e2586fab4a4ea95a582901d0d279316a
Yuri Adriel <yuriadriel.43@gmail.com>
2022-06-26 21:20:20 -0300

    fix: getMonth in helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,6 @@
 export const formatDate = (date) => {
   const data = new Date(date);
-  return data.getDate() + "/" + data.getMonth() + 1 + "/" + data.getFullYear();
+  return data.getDate() + "/" + data.getMonth() + "/" + data.getFullYear();
 };
 export const slugify = (text) => {
   return text.toString().normalize("NFKD").trim();
